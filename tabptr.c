c969e69e7b8f1e22006feecc0a7950afea9fc5b5
Rémi COHEN SCALI <remi@cohenscali.net>
2023-10-25 15:13:10 +0200

    Added new examples source code files
    Added tabptr.c
    Added echange.c/echangeadr.c
    Added constchars.c

---
+++ tabptr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init (int *tab, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		tab[i] = i + 1;
+	return;
+}
+
+int main()
+{
+	int i, n = 5;
+	int *tab;
+	tab = (int*)malloc(n * sizeof(int));
+	init(tab, n);
+	for (i = 0; i < n; i++)
+      printf("Element tab[%d] = %d\n", i, tab[i]);
+}
