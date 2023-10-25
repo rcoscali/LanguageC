3edbe1f77e8c46e6fdb9f7c6fc2e71fdea2edd87
Rémi COHEN SCALI <remi@cohenscali.net>
2023-10-25 22:20:48 +0200

    Updated programs
    Added add.c
    Added bsearch.c
    Updated CMakeLists.txt

---
+++ add.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int add(int,...);
+
+int add(int nb,...)
+{
+  int res = 0;
+  int i;
+  va_list liste_parametres;
+  va_start(liste_parametres, nb);
+  for (i = 0; i < nb; i++)
+    res += va_arg(liste_parametres, int);
+  va_end(liste_parametres);
+  return(res);
+}
+
+int main(void)
+{
+  printf("%d\n", add(4,10,2,8,5));
+  printf("%d\n", add(6,10,15,5,2,8,10));
+  return(EXIT_SUCCESS);
+}
